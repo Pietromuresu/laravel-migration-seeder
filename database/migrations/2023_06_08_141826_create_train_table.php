82270e1820331cb6c408e42a234f6cc8f90582dc
Pietromuresu <muresupietro66@gmail.com>
2023-06-08 17:01:24 +0200

    Migrate create_train_table

--- database/migrations/2023_06_08_141826_create_train_table.php
+++ database/migrations/2023_06_08_141826_create_train_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -13,9 +14,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('train', function (Blueprint $table) {
+        Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_hour');
+            $table->dateTime('arrival_hour');
+            $table->integer('train_number')->unique();
+            $table->tinyInteger('carriages_number');
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_deleted')->default(0);
+
         });
     }
 
@@ -26,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('train');
+        Schema::dropIfExists('trains');
     }
 };
